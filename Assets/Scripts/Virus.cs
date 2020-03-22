5751645785b276e1967050d90f0a9f947891a347
nicokim <nicokim1997@outlook.com>
2020-03-22 04:04:45 -0300

    Making the virus stick to the hand

---
+++ Assets/Scripts/Virus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    //config params
+    [SerializeField] Paddle hand;
+    //state
+    Vector2 handToVirusVector;
+    void Start()
+    {
+        handToVirusVector = transform.position - hand.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 handPos = new Vector2(hand.transform.position.x,hand.transform.position.y);
+        transform.position = handPos + handToVirusVector;
+    }
+}
