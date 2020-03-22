ee77654f892cd85a38e553a715db2b8a9662d182
nicokim <nicokim1997@outlook.com>
2020-03-22 04:27:41 -0300

    Launch the virus on click

--- Assets/Scripts/Virus.cs
+++ Assets/Scripts/Virus.cs
@@ -6,8 +6,11 @@ public class Virus : MonoBehaviour
 {
     //config params
     [SerializeField] Paddle hand;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
     //state
     Vector2 handToVirusVector;
+    bool hasStarted = false;
     void Start()
     {
         handToVirusVector = transform.position - hand.transform.position;
@@ -15,6 +18,23 @@ public class Virus : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if(!hasStarted)
+        {
+            LockVirusToHand();
+            LaunchOnClick();
+        }
+    }
+
+    private void LaunchOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
+        }
+    }
+    private void LockVirusToHand()
     {
         Vector2 handPos = new Vector2(hand.transform.position.x,hand.transform.position.y);
         transform.position = handPos + handToVirusVector;
