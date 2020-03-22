f661d2a8974d58c8377feee4273b25f042e52618
nicokim <nicokim1997@outlook.com>
2020-03-22 03:38:03 -0300

    Hand (Paddle) movement with mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits,0.25f);
+        transform.position = paddlePos;
+    }
+}
